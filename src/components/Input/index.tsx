930b472a6c3ec17efe214be8157468c21f6ac3e4
Nataly Rojas <natalyrojasmoreno@Nataly-Rojas-2.local>
2024-02-25 21:03:49 -0500

    FEAT: Blur images png to webp

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -5,6 +5,7 @@ export const Input = ({ textLabel, type, className, name, value, isValid, onChan
     <div className="relative w-full">
       <input
         name={name}
+        aria-labelledby={name}
         onChange={onChange}
         type={type}
         value={value}
@@ -17,6 +18,7 @@ export const Input = ({ textLabel, type, className, name, value, isValid, onChan
       />
 
       <p
+        id={name}
         className="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-4 z-10 origin-[0] start-2.5 peer-focus:text-gray-600 peer-focus:dark:text-gray-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto">
         {textLabel}
       </p>
